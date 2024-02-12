3ede895608a92fe610d22d240a19a575fbad187a
Kushagra Agrawal <agrawalkushagra29@gmail.com>
2024-02-12 16:24:52 +0530

    addded fetchUserPosts

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
+import Thread from "../models/thread.model";
 
 interface Params{
     userId: string;
@@ -59,4 +60,33 @@ export async function fetchUser(userId:string) {
     catch (error:any) {
         throw new Error(`Failed to fetch User: ${error.message}`)
     }
+}
+
+export async function fetchUserPosts(userId: string) {
+  try{
+    connectToDB();
+
+    //find all the threads authored by user with the given userId
+    const threads = await User.findOne({
+      id: userId
+    }).populate({
+      path: "threads",
+      model: Thread,
+      populate: {
+        path: 'children',
+        model: Thread,
+        populate: {
+          path: 'author',
+          model: User,
+          select: 'name image id',
+        }
+      }
+    });
+
+    return threads;
+  }
+  catch(error: any){
+    console.log("Error fetching user threads",error);
+    throw error;
+  }
 }
