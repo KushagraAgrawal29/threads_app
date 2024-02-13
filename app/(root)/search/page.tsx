5804d1f8caebd8d3ed0c4e8e0703e438924db06b
Kushagra Agrawal <agrawalkushagra29@gmail.com>
2024-02-13 18:20:01 +0530

    initialised routes

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import UserCard from "@/components/cards/UserCard";
+import SearchBar from "@/components/shared/Searchbar";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import React from "react";
 
-const page = () => {
+const page = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return null;
+  }
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) {
+    redirect("/");
+  }
+
+  //Fetch Users
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+  });
+
   return (
     <section>
-        <h1 className='head-text mb-10 '>Search</h1>
-    </section>
-  )
-}
+      <h1 className="head-text mb-10 ">Search</h1>
 
-export default page
+      <SearchBar routeType="search" />
+
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result">No Results</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType="User"
+              />
+            ))}
+          </>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default page;
