a923810a4139e00faa35a3840168b373c6da4cdc
Kushagra Agrawal <agrawalkushagra29@gmail.com>
2024-02-13 18:20:29 +0530

    initialised

---
+++ components/shared/Searchbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    routeType: string,
+}
+
+const SearchBar = ({ routeType }: Props) => {
+  return (
+    <div>
+      Search Bar
+    </div>
+  )
+}
+
+export default SearchBar
